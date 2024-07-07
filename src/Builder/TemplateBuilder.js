cf1d2a01e248c5bd3b6f7d46dac283553e426a0d
soothsayer136 <bikrum.katwal@gmail.com>
2024-07-07 22:29:46 +0545

    changes

---
+++ src/Builder/TemplateBuilder.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Playground from './Playground'
+
+function TemplateBuilder() {
+    return (
+        <div>
+            <Playground />
+        </div>
+    )
+}
+
+export default TemplateBuilder
